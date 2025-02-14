06daa54306148937184ca49527cded25f4ffae13
kuladeep <vuppukuladeep@gmail.com>
2025-02-14 19:40:34 +0530

    removed unwanted function

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from .views import hello
 from .views import UserSubmissionViewSet
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
@@ -10,7 +9,6 @@ router.register(r'submissions', UserSubmissionViewSet, basename='submission')
 
 
 urlpatterns = [
-    path('hello/', hello, name='hello-api'),
     path('api/', include(router.urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # Schema JSON
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
--- backend/api/views.py
+++ backend/api/views.py
@@ -9,13 +9,4 @@ class UserSubmissionViewSet(viewsets.ModelViewSet):
     queryset = UserSubmision.objects.all()
     serializer_class = UserSubmissionSerializer
 
-@api_view(['POST'])
-def hello(request):
-    data = request.data  # DRF handles JSON parsing automatically
-    username = data.get('name')
-    password = data.get('pass')
 
-    if not username or not password:
-        return Response({"error": "Username and password are required"}, status=400)
-
-    return Response({"message": "Data received", "username": username, "password": password})
