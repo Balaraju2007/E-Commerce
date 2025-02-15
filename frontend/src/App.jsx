211f7d08d216ce3c3e44d23b3cd3654b3f5e8b78
laptop-h4ridrjt balar <laptop-h4ridrjt.balar@default.com>
2025-02-15 14:12:20 +0530

    added registartion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import Registration from './loginPages/Registration'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [user, setUser] = useState(null)
-  const [pass, setPass] = useState(null)
-
-  const handleName = (event) => {
-    setUser(event.target.value)
-  }
-
-  const handlePass = (event) => {
-    setPass(event.target.value)
-  }
-
-  const call = async (event) =>{
-    event.preventDefault(); // Prevents page reload
-    try{
-    const response = await fetch('http://127.0.0.1:8000/api/submissions/',{
-      method: 'POST',
-      headers:{
-        'Content-type':'application/json'
-      },
-      body:JSON.stringify({username:user, password:pass})
-    })
-    const result = await response.json()
-    console.log(result)
-  }
-  catch(error){
-    console.log(error)
-  }
-  }
   return (
-    <>
-      <form onSubmit={call}>
-        <input type='text' onChange={handleName} />
-        <input type='text' onChange={handlePass} />
-        <button type='submit' >submit</button>
-      </form>
-    </>
+    <div className='flex justify-center align-center'>
+      <Registration />
+    </div>
   )
 }
 
---
+++ frontend/src/loginPages/Registration.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react'
+import "./registration.css";
+
+const Registration = () => {
+    const [count, setCount] = useState(0)
+    const [user, setUser] = useState(null)
+    const [pass, setPass] = useState(null)
+
+    const handleName = (event) => {
+        setUser(event.target.value)
+    }
+
+    const handlePass = (event) => {
+        setPass(event.target.value)
+    }
+
+    const call = async (event) => {
+        event.preventDefault(); // Prevents page reload
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/submissions/', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({ username: user, password: pass })
+            })
+            const result = await response.json()
+            console.log(result)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    return (
+        <>
+            <form className='flex justify-center' onSubmit={call}>
+                <div className='form'>
+                    <div className='username'><label>UserName : </label><input type='text' onChange={handleName} required /> </div>
+                    <div className='passsword'><label>Password : </label><input type='text' onChange={handlePass} required /> </div>
+                    <div className='email'><label>Email : </label><input type='emmail' onChange={handlePass} required /> </div>
+                    <button type='submit' >submit</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default Registration
