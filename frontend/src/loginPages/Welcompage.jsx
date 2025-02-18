22d80cf0556b69ced9ef494cab919231bed86446
laptop-h4ridrjt balar <laptop-h4ridrjt.balar@default.com>
2025-02-18 22:55:26 +0530

    add css to welcome page

--- frontend/src/loginPages/Welcompage.jsx
+++ frontend/src/loginPages/Welcompage.jsx
@@ -10,10 +10,30 @@ const Header = () => {
             </div>
             <div className='signinup'>
                 <p>Contact us</p>
-                <p>Sign in</p>
+                <p style={{ color: 'blue',}}>Sign in</p>
                 <p>Sign up</p>
             </div>
         </header>
+    )
+}
+
+const Body = () => {
+    return(
+        <div className='container'>
+            <div className='content'><h1>Find & Sell Old <br></br>Semester Books Easily!</h1><br></br>
+            <p>Buy books at low prices and sell your used books effortlessly</p><br></br>
+            <p style={{ backgroundColor: 'blue', fontSize: '20px', display: 'inline', padding: '10px', borderRadius: '10px'}}>Get Started</p></div>
+            <div className='image'><img className="welcomePageImage"src="jrff">
+            </img></div>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer style={{ textAlign: 'center', fontSize: '20px'}}>
+           if you have anything conatct us +91 xxxxx xxxxx
+        </footer>
     )
 }
 const Welcomapage = () => {
@@ -22,7 +42,8 @@ const Welcomapage = () => {
         <>
             <div className='bodypart'>
                 <Header />
-                {/* <Body /> */}<div>balaaaaaaaa</div>
+                <Body />
+                <Footer />
             </div>
         </>
     )
