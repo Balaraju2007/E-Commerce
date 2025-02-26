0857ac07edfd7fd4d19c17bd2b62768ba35433cf
vuppu kuladeep <vuppukuladeep@gmail.com>
2025-02-26 07:56:36 +0530

    returning jwt token when user logs in

--- fastapi/app/routers/auth.py
+++ fastapi/app/routers/auth.py
@@ -3,6 +3,10 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from app import models, schemas
 from app.database import get_db
+from ..tokens import create_access_token
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status
 
 BASE_URL = "http://127.0.0.1:8000"
 router = APIRouter()
@@ -12,6 +16,18 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @router.post("/")
 def login(data:schemas.LoginRequest, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == data.email).first()
-    if user and pwd_cxt.verify(data.password, user.password):
-        return {"message": "Login successful", "user": {"id": user.id, "username": user.full_name, "email": user.email, "profile_image": f"{BASE_URL}/{user.profile_image}"}}
-    raise HTTPException(status_code=400, detail="Invalid credentials")
+    # if user and pwd_cxt.verify(data.password, user.password):
+    #     return {"message": "Login successful", "user": {"id": user.id, "username": user.full_name, "email": user.email, "profile_image": f"{BASE_URL}/{user.profile_image}"}}
+    # raise HTTPException(status_code=400, detail="Invalid credentials")
+    
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    if not pwd_cxt.verify(data.password, user.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=timedelta(minutes=30))
+    return {"access_token": access_token, "token_type": "bearer",  "user": {"id": user.id, "username": user.full_name, "email": user.email, "profile_image": f"{BASE_URL}/{user.profile_image}"}}
+    
+    
+
+
+
--- fastapi/app/schemas.py
+++ fastapi/app/schemas.py
@@ -25,3 +25,12 @@ class UserResponse(BaseModel):
 class LoginRequest(BaseModel):
     email: EmailStr
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
