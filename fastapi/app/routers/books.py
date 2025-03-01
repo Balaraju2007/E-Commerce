3d78fda676db1dfe1392b2ab9f845d6494536f27
vuppu kuladeep <vuppukuladeep@gmail.com>
2025-03-01 19:10:27 +0530

    added books api

--- fastapi/app/routers/books.py
+++ fastapi/app/routers/books.py
@@ -119,11 +119,124 @@ async def create_book(
 @router.delete('/{book_id}', response_model=dict)
 def delete_book(book_id:int, db: Session = Depends(get_db)):
     book = db.query(models.Book).filter(models.Book.book_id == book_id).first()
-
+    
     if not book:
         return {'message' : 'no book there in my db with such id'}
     
     db.delete(book)
     db.commit()
-
     return { 'message' : 'book deleted successfully'}
+
+
+@router.get('/{book_id}', response_model=dict)
+def delete_book(book_id:int, db: Session = Depends(get_db)):
+    book = (
+        db.query(
+            models.Book,
+            models.User.full_name.label("seller_name"),
+            models.Author.author_name.label("author_name"),
+            models.Genre.genre_name.label("genre_name"),
+            models.Publisher.publisher_name.label("publisher_name"),
+        )
+        .join(models.User, models.User.user_id == models.Book.seller_id)
+        .join(models.Author, models.Author.author_id == models.Book.author_id)
+        .join(models.Genre, models.Genre.genre_id == models.Book.genre_id)
+        .join(models.Publisher, models.Publisher.publisher_id == models.Book.publisher_id)
+        .filter(models.Book.book_id == book_id)
+        .first()
+    )
+    
+    if not book:
+        return {'message' : 'no book there in my db with such id'}
+    
+    return  {
+            "book_id": book.Book.book_id,
+            "book_name": book.Book.book_name,
+            "seller_name": book.seller_name,
+            "author_name": book.author_name,
+            "price": book.Book.price,
+            "quantity": book.Book.quantity,
+            "genre_name": book.genre_name,
+            "publisher_name": book.publisher_name,
+            "picture": f"{BASE_URL}/uploads/books/{book.Book.picture}"
+        }
+
+
+
+
+@router.put('/{book_id}', response_model=dict)
+async def update_book(
+    book_id:int,
+    db: Session = Depends(get_db),
+    book_name: str = Form(...),
+    seller_name: str = Form(...),
+    quantity: int = Form(...),
+    author_name: str = Form(...),
+    publisher_name: str = Form(...),
+    genre_name: str = Form(...),
+    price: int = Form(...),
+    picture: UploadFile = File(...)
+):
+    book = db.query(models.Book).filter(models.Book.book_id == book_id).first()
+    
+    if not book:
+        return {'message' : 'no book there in my db with such id'}
+    
+    author = db.query(models.Author).filter(models.Author.author_name == author_name).first()
+    if not author:
+        author = models.Author(author_name = author_name)
+        db.add(author)
+        db.commit()
+        db.refresh(author)
+    
+    publisher = db.query(models.Publisher).filter(models.Publisher.publisher_name == publisher_name).first()
+    if not publisher:
+        publisher = models.Publisher(publisher_name = publisher_name)
+        db.add(publisher)
+        db.commit()
+        db.refresh(publisher)
+
+    genre = db.query(models.Genre).filter(models.Genre.genre_name == genre_name).first()
+    if not genre:
+        genre = models.Genre(genre_name = genre_name)
+        db.add(genre)
+        db.commit()
+        db.refresh(genre)
+
+    seller = db.query(models.User).filter(models.User.full_name == seller_name).first()
+    if not seller:
+        return {'message': 'invalid user adding books'}
+    seller_id = seller.user_id
+
+    picture_filename = f"{book_name.replace(' ', '_')}.jpg"
+    picture_path = os.path.join(UPLOAD_DIR, picture_filename)
+
+    with open(picture_path, "wb") as buffer:
+        buffer.write(await picture.read())
+
+    # Create the new book entry
+    new_book = models.Book(
+        book_name=book_name,
+        seller_id=seller_id,
+        author_id=author.author_id,
+        publisher_id=publisher.publisher_id,
+        genre_id=genre.genre_id,
+        price=price,
+        quantity=quantity,
+        picture=picture_filename,
+    )
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+
+    return {
+        "book_id":new_book.book_id,
+        "book_name":new_book.book_name,
+        "seller_name":seller_name,
+        "author_name":author.author_name,
+        "price":new_book.price,
+        "quantity":new_book.quantity,
+        "genre_name":genre.genre_name,
+        "publisher_name":publisher.publisher_name,
+        "picture":f"{BASE_URL}/uploads/books/{new_book.picture}"
+    }
