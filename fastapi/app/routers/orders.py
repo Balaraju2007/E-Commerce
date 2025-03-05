b81682ae4182d87b2fd760f249df01ccba2e11a8
vuppu kuladeep <vuppukuladeep@gmail.com>
2025-03-05 06:27:04 +0530

    added orders api's

--- fastapi/app/routers/orders.py
+++ fastapi/app/routers/orders.py
@@ -34,3 +34,76 @@ def place_order(
             'book_id': book_id,
             'quantity': quantity
         }
+        
+    cart = db.query(models.Cart).filter(models.Cart.user_id == user_id).first()
+    if not cart or not cart.cart_items:
+        return {'message' : 'your cart is empty nothing to order'}
+    
+    order = models.Order(user_id = user_id, order_date = datetime.utcnow())
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+    
+    for cart_item in cart.cart_items:
+        order_item = models.OrderItem(order_id = order.order_id, book_id = cart_item.book_id, quantity = cart_item.quantity)
+        db.add(order_item)
+    
+    db.commit()
+    
+    return {
+        "message": "Order placed successfully (Cart Order)",
+        "order_id": order.order_id
+    }
+    
+@router.get('/{user_id}')
+def get_orders(user_id: int, db: Session = Depends(get_db)):
+    """ Get all orders placed by a user"""
+    
+    orders = db.query(models.Order).filter(models.Order.user_id == user_id).all()
+    if not orders:
+        return {"message": "No orders found"}
+
+    return [
+        {
+            "order_id": order.order_id,
+            "order_date": order.order_date
+        }
+        for order in orders
+    ]
+    
+@router.get('details/{order_id}')
+def ger_order_details(order_id: int, db: Session = Depends(get_db)):
+    """ return details for given order id"""
+    
+    order = db.query(models.Order).filter(models.Order.order_id == order_id).first()
+    
+    if not order:
+        return {'message': 'order not found'}
+    
+    order_items = db.query(models.OrderItem).filter(models.OrderItem.order_id == order_id).all()
+    
+    return {
+        "order_id": order.order_id,
+        "order_date": order.order_date,
+        "books": [
+            {
+                "book_id": item.book_id,
+                "quantity": item.quantity
+            }
+            for item in order_items
+        ]
+    }
+    
+
+@router.delete("/{order_id}")
+def delete_order(order_id: int, db: Session = Depends(get_db)):
+    """✅ Deletes an order and its items"""
+    
+    order = db.query(models.Order).filter(models.Order.order_id == order_id).first()
+    if not order:
+        return{'message': "Order not found"}
+
+    db.delete(order)
+    db.commit()
+    
+    return {"message": f"Order {order_id} deleted successfully"}
