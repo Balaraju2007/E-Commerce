4c7fbe077c8e89b4abb2e13e559fbd52498c7150
vuppu kuladeep <vuppukuladeep@gmail.com>
2025-03-15 12:21:15 +0530

    added the cartpage css

--- frontend/src/serachCartOrder/Cart.jsx
+++ frontend/src/serachCartOrder/Cart.jsx
@@ -118,7 +118,7 @@ const Cart = () => {
       navigate(`/singleBookDetails/${value}`)
     }
   return (
-    <div className='homeContainer'>
+    <div className='cart-container'>
       <Header />
       <div className='total-carts'>
         <p>{status ? `You have ${cartBooks.cart_items?.length} books in your cart` : "No books in cart"}</p>
@@ -129,15 +129,7 @@ const Cart = () => {
       {cartBooks.cart_items?.length > 0 ? (
         <div className='allDisplayedBooks'>
           {cartBooks.cart_items.map((item) => (
-            <div key={item.book_id} className='book' onClick={(e)=>{handleBook(e, item.book_id)}}>
-              <div className="book-seller">
-                <img 
-                  onClick={(e) => handleNavigate(item.book_detals.seller_id, e)}
-                  src={item.book_detals.seller_profile}
-                  alt={item.book_detals.seller_name}
-                  className="seller-image"
-                />
-                <p onClick={(e) => handleNavigate(item.book_detals.seller_id, e)} className='seller-Name'>{item.book_detals.seller_name}</p>
+            <div key={item.book_id} className='cart-book' onClick={(e)=>{handleBook(e, item.book_id)}}>
                 <svg 
                   onClick={(e) => handleDeleteCart(e, item.cart_item_id)} 
                   xmlns="http://www.w3.org/2000/svg" 
@@ -150,7 +142,6 @@ const Cart = () => {
                   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
                   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
                 </svg>
-              </div>
               <p>Book Name: {item.book_name}</p>
               <img src={item.book_detals.picture} className='image' alt={item.book_name} />
               <p><span>by</span> {item.book_detals.author_name}</p>
