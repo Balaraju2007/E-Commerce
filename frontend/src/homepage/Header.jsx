f635ebe75f1009baa089c12d1b7780b598ec0217
vuppu kuladeep <vuppukuladeep@gmail.com>
2025-03-15 11:35:25 +0530

    changed header  css

--- frontend/src/homepage/Header.jsx
+++ frontend/src/homepage/Header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useBookData } from '../serachCartOrder/BookDataContext'; // Import the custom hook
 import './header.css';
+import { FiSearch } from 'react-icons/fi';
 
 const Header = () => {
   const [query, setQuery] = useState('');
@@ -46,82 +47,42 @@ const Header = () => {
     setQuery(e.target.value);
   };
 
-  return (
-    <div className='HeaderContainer'>
-      <div className='nameSearch'>
-        <h2
-          style={{ marginTop: '6%', fontSize: '2rem', fontWeight: 'inherit' }}
+  return(
+        <header className="header">
+        <h1 className="title"
+        onClick={() => {
+                navigate('/home');
+                   }}>E-Commerce</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Search for books..." className="search-input" />
+          <button className="search-button">
+            <FiSearch className="search-icon" />
+          </button>
+        </div>
+        <div className="icons">
+          <i className="bi bi-plus-circle icon" title="Add Book"onClick={() => { navigate('/addbook') }}></i>
+          <i className="bi bi-list icon" title="My Orders"
           onClick={() => {
-            navigate('/home');
-          }}
-        >
-          Book Bazaar
-        </h2>
-        <h2 className='Input svg'>
-          <input
-            value={query}
-            onChange={handleSearch}
-            onClick={() => {
-              navigate('/search');
-            }}
-            placeholder='--Search---'
-          />
-        </h2>
-      </div>
-
-      <div className='profileCartOrder'>
-        <h2 className='svg'>
-          <svg
-            onClick={() => {
-              navigate('/cart');
-            }}
-            xmlns='http://www.w3.org/2000/svg'
-            width='25'
-            height='25'
-            fill='currentColor'
-            className='bi bi-cart'
-            viewBox='0 0 16 16'
-          >
-            <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2' />
-          </svg>
-        </h2>
-        <h2 className='svg'>
-          <svg
-            onClick={() => {
-              navigate('/order');
-            }}
-            xmlns='http://www.w3.org/2000/svg'
-            width='25'
-            height='25'
-            fill='currentColor'
-            className='bi bi-bag'
-            viewBox='0 0 16 16'
-          >
-            <path d='M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z' />
-          </svg>
-        </h2>
-        <h2 className='svg' onClick={() => { navigate('/addbook') }}>
-          <svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='currentColor' className='bi bi-plus-circle' viewBox='0 0 16 16'>
-            <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16' />
-            <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4' />
-          </svg>
-        </h2>
-        <h2 className='profile'>
-          {profile ? (
-            <img
-              src={profile}
+                         navigate('/order');
+                       }}></i>
+          <i className="bi bi-cart icon" title="Cart"
+          onClick={() => {
+                         navigate('/cart');
+                       }}></i>
+          <i>{profile ? (
+             <img
+               src={profile}
               onClick={() => {
-                navigate(`/profile/${localStorage.getItem('user_id')}`);
-              }}
-              className='svg'
-              alt='Profile'
+                 navigate(`/profile/${localStorage.getItem('user_id')}`);
+               }}
+               className='profile-icon'
+               alt='Profile'
             />
-          ) : (
-            <span>No Profile</span>
-          )}
-        </h2>
-      </div>
-    </div>
+           ) : (
+            <i className="bi bi-person icon" title="Profile"></i>
+           )} </i>
+        </div>
+      </header>
   );
 };
 
